0bd64f16b03fa68d86aedcf77d3c3b8b823821f0
salahhus <salahiddinov@gmail.com>
2024-10-23 23:47:29 +0200

    git initial

--- CarStore.Mapper/obj/Debug/net8.0/CarStore.Mapper.AssemblyInfo.cs
+++ CarStore.Mapper/obj/Debug/net8.0/CarStore.Mapper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarStore.Mapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64eaac2378dd11c0d580524a9cbb1d3ee3ce3dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarStore.Mapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarStore.Mapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
